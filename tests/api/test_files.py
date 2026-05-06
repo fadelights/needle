d665a215b4a01aafbf1b17e82d5f950654b1330c
Mani <mani@cartesianlabs.io>
2026-05-06 17:17:56 +0330

    chore: restructure test dir

---
+++ tests/api/test_files.py
@@ -0,0 +1,21 @@
+import io
+
+
+def test_list_files():
+    pass
+
+
+def test_upload_file():
+    pass
+
+
+def test_get_file_content():
+    pass
+
+
+def test_update_file_content():
+    pass
+
+
+def test_delete_file():
+    pass
